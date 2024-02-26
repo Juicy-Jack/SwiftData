220420102e1134ad23f5dce4d615a9bad165e082
Juicy-Jack <71324263+Juicy-Jack@users.noreply.github.com>
2024-02-26 20:08:31 +0300

    Version 1

---
+++ HWS - SwiftData Starter Project/iTour/iTour/Sight.swift
@@ -0,0 +1,18 @@
+//
+//  Sight.swift
+//  iTour
+//
+//  Created by Furkan Doğan on 4.10.2023.
+//
+
+import SwiftData
+import Foundation
+
+@Model
+class Sight {
+    var name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+}
